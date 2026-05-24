a52146ae8c7a1628cd06501ae9efa6d532af1358
Khamzat <yunusov_hamzat@mail.ru>
2026-05-24 23:30:44 +0300

    Initial Commit

---
+++ DevSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevSearch
+//
+//  Created by Хамзат Юнусов on 24.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevSearch/DevSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevSearchApp.swift
+//  DevSearch
+//
+//  Created by Хамзат Юнусов on 24.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DevSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
